6a40325e97b52642a211c0f83ba5bcbf3f431d94
Saloni Goyal <SaloniGoyal@users.noreply.github.com>
2020-11-21 09:43:35 +0530

    Function to add two integers

---
+++ add.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using std::cout;
+
+int add (int x, int y)
+{
+    return x + y;
+}
+
+int main()
+{
+    int total = add(3, 4);
+    cout << "3 + 4 is " << total << '\n';
+    return 0;
+}
+
+
+
+
